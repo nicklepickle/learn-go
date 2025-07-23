6675349bd5a74908096369a4ccbc758f6db4cca3
VanBrunt, Nick <NVanBrunt@kennedy-center.org>
2025-07-23 11:52:51 -0400

    Added user functions

--- http/server.go
+++ http/server.go
@@ -5,15 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/mail"
-	"regexp"
-	"time"
-
-	"golang.org/x/crypto/bcrypt" // import bcrypt
 )
 
-const bcryptCost int = 8
-const minPassLen int = 8
 const port int = 8080
 
 type JsonResponse struct {
@@ -34,71 +27,7 @@ func (r *JsonResponse) write(w http.ResponseWriter) error {
 }
 
 func loginHandler(res http.ResponseWriter, req *http.Request) {
-	username := req.PostFormValue("user")
-	pw := req.PostFormValue("password")
-
-	user, exists := Users[username]
-	if exists && user.Status == 1 {
-		//fmt.Printf("USERS user=%s password=%s\n", user.UserName, user.Hash)
-		err := bcrypt.CompareHashAndPassword([]byte(user.Hash), []byte(pw))
-		if err == nil {
-			user.Seen = time.Now()
-			err = user.save()
-			if err != nil {
-				log.Println(err.Error())
-			}
-			response := JsonResponse{
-				Data:   user,
-				Status: 200,
-			}
-			response.write(res)
-			return
-		}
-	}
-
-	errors := []string{"Log in failed"}
-	response := JsonResponse{
-		Errors: errors,
-		Status: 401,
-	}
-	response.write(res)
-}
-
-func joinHandler(res http.ResponseWriter, req *http.Request) {
-	username := req.PostFormValue("user")
-	email := req.PostFormValue("email")
-	pw1 := req.PostFormValue("password")
-	pw2 := req.PostFormValue("confirm")
-
-	// validate
-	errors := []string{}
-
-	match, _ := regexp.MatchString("^[a-zA-Z0-9_]+$", username)
-	if !match {
-		errors = append(errors, "User name contains non-alphanumeric characters")
-	}
-
-	_, exists := Users[username]
-	if exists {
-		errors = append(errors, fmt.Sprintf("User name %s is already taken", username))
-	}
-
-	if len(pw1) < minPassLen {
-		errors = append(errors, fmt.Sprintf("Password is less than %d characters", minPassLen))
-	}
-
-	if pw1 != pw2 {
-		errors = append(errors, "Password and confirmation do not match")
-	}
-	_, err := mail.ParseAddress(email)
-	if err != nil {
-		errors = append(errors, fmt.Sprintf("Email addess %s is invalid", email))
-	}
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(pw1), bcryptCost)
-	if err != nil {
-		errors = append(errors, fmt.Sprintf("Password is invalid %s", err.Error()))
-	}
+	user, errors := AuthenticateUser(req)
 
 	if len(errors) > 0 {
 		response := JsonResponse{
@@ -106,45 +35,34 @@ func joinHandler(res http.ResponseWriter, req *http.Request) {
 			Status: 401,
 		}
 		response.write(res)
-		return
+	} else {
+		response := JsonResponse{
+			Data:   user,
+			Status: 200,
+		}
+		response.write(res)
 	}
+}
 
-	//fmt.Printf("POST user=%s email=%s hash=%s\n", username, email, hash)
+func joinHandler(res http.ResponseWriter, req *http.Request) {
+	user, errors := RegisterUser(req)
 
-	user := User{
-		UserId:   len(Users) + 1, //  let db do this
-		UserName: username,
-		Email:    email,
-		Hash:     string(hash),
-		Created:  time.Now(),
-		Seen:     time.Now(),
-		Status:   1,
-	}
-	err = user.save()
-
-	if err != nil {
-		log.Println(err.Error())
-		// something else went wrong
-		errors = append(errors, err.Error())
+	if len(errors) > 0 {
 		response := JsonResponse{
 			Errors: errors,
 			Status: 401,
 		}
 		response.write(res)
-		return
+	} else {
+		response := JsonResponse{
+			Data:   user,
+			Status: 200,
+		}
+		response.write(res)
 	}
-
-	// happy path
-	response := JsonResponse{
-		Data:   user,
-		Status: 200,
-	}
-	response.write(res)
 }
 
 func main() {
-	ReadUsers()
-
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 	http.HandleFunc("/login", loginHandler)
--- http/user.go
+++ http/user.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/mail"
 	"os"
+	"regexp"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+const bcryptCost int = 8
+const minPassLen int = 8
 
 var Users map[string]User = make(map[string]User)
 
@@ -20,11 +30,94 @@ type User struct {
 
 func (u *User) save() error {
 	Users[u.UserName] = *u
-	err := WriteUsers()
+	err := writeUsers()
 	return err
 }
 
-func ReadUsers() error {
+func AuthenticateUser(req *http.Request) (*User, []string) {
+	username := req.PostFormValue("user")
+	pw := req.PostFormValue("password")
+	if len(Users) == 0 {
+		err := readUsers()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+
+	user, exists := Users[username]
+	if exists && user.Status == 1 {
+		//fmt.Printf("USERS user=%s password=%s\n", user.UserName, user.Hash)
+		err := bcrypt.CompareHashAndPassword([]byte(user.Hash), []byte(pw))
+		if err == nil {
+			user.Seen = time.Now()
+			err = user.save()
+			if err != nil {
+				log.Println(err.Error())
+			}
+			return &user, nil
+		}
+	}
+
+	return nil, []string{"Log in failed"}
+}
+
+func RegisterUser(req *http.Request) (*User, []string) {
+	username := req.PostFormValue("user")
+	email := req.PostFormValue("email")
+	pw1 := req.PostFormValue("password")
+	pw2 := req.PostFormValue("confirm")
+
+	// validate
+	errors := []string{}
+
+	match, _ := regexp.MatchString("^[a-zA-Z0-9_]+$", username)
+	if !match {
+		errors = append(errors, "User name contains non-alphanumeric characters")
+	}
+
+	_, exists := Users[username]
+	if exists {
+		errors = append(errors, fmt.Sprintf("User name %s is already taken", username))
+	}
+
+	if len(pw1) < minPassLen {
+		errors = append(errors, fmt.Sprintf("Password is less than %d characters", minPassLen))
+	}
+
+	if pw1 != pw2 {
+		errors = append(errors, "Password and confirmation do not match")
+	}
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		errors = append(errors, fmt.Sprintf("Email addess %s is invalid", email))
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(pw1), bcryptCost)
+	if err != nil {
+		errors = append(errors, fmt.Sprintf("Password is invalid %s", err.Error()))
+	}
+
+	user := User{
+		UserId:   len(Users) + 1, //  let db do this
+		UserName: username,
+		Email:    email,
+		Hash:     string(hash),
+		Created:  time.Now(),
+		Seen:     time.Now(),
+		Status:   1,
+	}
+	err = user.save()
+	if err != nil {
+		errors = append(errors, fmt.Sprintf("Could not save user %s", err.Error()))
+	}
+
+	if len(errors) > 0 {
+		return nil, errors
+	}
+	return &user, nil
+}
+
+func readUsers() error {
 	path, err := os.Getwd()
 	if err != nil {
 		return err
@@ -42,7 +135,7 @@ func ReadUsers() error {
 	return nil
 }
 
-func WriteUsers() error {
+func writeUsers() error {
 	path, err := os.Getwd()
 	if err != nil {
 		return err
